3327a4faf895d9399bbb01649876ea5b0704e364
Fatima Guzman <fguzman1230@gmail.com>
2022-08-17 12:22:45 -0600

    Moved lightbox plugin code from index.html to app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-baguetteBox.run(".gallery");
+//Lightbox plugin code
+
+window.addEventListener("load", function () {
+  baguetteBox.run(".gallery");
+});
+
+const input = document.querySelector("#search-form");
+
+input.addEventListener("keyup", () => {
+  console.log(input.value);
+});
